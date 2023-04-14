e902a4e62a4124498d5f52e48b73d79c1dfddc22
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-14 19:25:07 +0700

    Update ProgramUtama.py

--- ProgramUtama.py
+++ ProgramUtama.py
@@ -3,3 +3,10 @@ def length(x):
     for i in range (103):
         if x[i]!=str('inf'): sum+=1
     return sum
+
+def mappend(arr, c):
+    new_arr = [None] * (length(arr) + 1)
+    for i in range(length(arr)):
+        new_arr[i] = arr[i]
+    new_arr[length(arr)] = c
+    return new_arr
