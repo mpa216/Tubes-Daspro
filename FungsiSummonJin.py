512c817e06a6a28a7cb592900617de2d2d5f735b
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-16 15:22:36 +0700

    Update FungsiSummonJin.py

--- FungsiSummonJin.py
+++ FungsiSummonJin.py
@@ -8,10 +8,9 @@ def jenis_jin(username_jintotal, username_jinpembangun, password_jinpembangun, u
     if x==1:
         print("Memilih jin “Pengumpul”.")
         uname_pengumpul = str(input("Masukkan username jin: "))
-        pass_pengumpul = str(input("Masukkan password jin: "))
         j=0
-        while j<length(username_jinpengumpul):
-            if username_jinpengumpul[j]==uname_pengumpul:
+        while j<length(username_jintotal):
+            if username_jintotal[j]==uname_pengumpul:
                 print(f'Username "{uname_pengumpul}" sudah diambil!')
                 uname_pengumpul = str(input("Masukkan username jin: "))
                 tambah = False
@@ -19,15 +18,15 @@ def jenis_jin(username_jintotal, username_jinpembangun, password_jinpembangun, u
             else:
                 tambah = True
             j+=1
+        pass_pengumpul = str(input("Masukkan password jin: "))
         if tambah:
                 return mappend(username_jinpengumpul, uname_pengumpul), mappend(username_jintotal, uname_pengumpul), mappend(password_jinpengumpul, pass_pengumpul)
     elif x==2:
         print("Memilih jin Pembangun.")
         uname_pembangun = str(input("Masukkan username jin: "))
-        pass_pembangun = str(input("Masukkan password jin: "))
         j=0
-        while j<length(username_jinpembangun):
-            if username_jinpembangun[j]==uname_pembangun:
+        while j<length(username_jintotal):
+            if username_jintotal[j]==uname_pembangun:
                 print(f'Username "{uname_pembangun}" sudah diambil!')
                 uname_pembangun = str(input("Masukkan username jin: "))
                 tambah = False
@@ -35,6 +34,7 @@ def jenis_jin(username_jintotal, username_jinpembangun, password_jinpembangun, u
             else:
                 tambah = True
                 j+=1
+        pass_pembangun = str(input("Masukkan password jin: "))
         if tambah:
                 return mappend(username_jinpembangun, uname_pembangun), mappend(username_jintotal, uname_pembangun), mappend(password_jinpembangun, pass_pembangun)
     else:
