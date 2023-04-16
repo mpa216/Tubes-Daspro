defbdd3c1c4de802a5905653bb8d8463c7b98f84
Reasondro <116406357+Reasondro@users.noreply.github.com>
2023-04-16 21:37:04 +0700

    coba Utama

--- ProgramUtama.py
+++ ProgramUtama.py
@@ -1,6 +1,12 @@
 def length(x):
-    i, sum=0,0
-    while i!=103:
-        if x[i]!=str('inf'): sum+=1
-        i+=1
+    sum=0
+    for i in range (103):
+        if x[i]!=float('inf'): sum+=1
     return sum
+
+def mappend(arr, c):
+    new_arr = [None] * (length(arr) + 1)
+    for i in range(length(arr)):
+        new_arr[i] = arr[i]
+    new_arr[length(arr)] = c
+    return new_arr
