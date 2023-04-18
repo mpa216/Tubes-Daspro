d4b28ce590966199fcd3a246aea34ada053638dc
Reasondro <116406357+Reasondro@users.noreply.github.com>
2023-04-18 21:41:18 +0700

    Update 1 HancurkanCandi

--- HancurkanCandi.py
+++ HancurkanCandi.py
@@ -1,18 +1,27 @@
 from ProgramUtama  import length
 
-jumlahCandi = length(arrCandi)
+arrCandi = [0 for i in range (103)] # arrCandi array berisi kumpulan candi, asumsi sudah penuh
+
+arrCandi[0] = 1 # hanya sebagai test case, tidak akan dimasukkan di versi akhir
+
+jumlahCandi = length(arrCandi) # sebagai variabel panjang array candi
 def HancurkanCandi(arrCandi):
     id = int(input("Masukkan ID candi: "))
     for i in range(jumlahCandi):
         if (arrCandi[i] == id):
             print(f"Apakah anda ingin menghancurkan candi ID: {id} (Y/N)?")
             jawab = input()
-            if (jawab == "Y"):
+            if (jawab == "Y") or (jawab == "y"):
                 arrCandi[i] = float('inf')
                 print("Candi telah berhasil dihancurkan.")
-            elif (jawab == "N"):
+            elif (jawab == "N") or (jawab == "n"):
                 print("Candi tidak jadi dihancurkan.")
         else:
             if (i == jumlahCandi-1):
                 print("Tidak ada candi dengan ID tersebut.")
+                break
 
+HancurkanCandi(arrCandi) #test case
+
+print(arrCandi) # test case untuk mengecek kondisi array terbaru
+print(length(arrCandi)) # test case untuk mengecek panjang array 
