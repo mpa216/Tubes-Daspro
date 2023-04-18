23701e7b12a6ff76d2e301164a31a0bb889ce6cd
Reasondro <116406357+Reasondro@users.noreply.github.com>
2023-04-18 21:49:22 +0700

    Update 3 HancurkanCandi

--- HancurkanCandi.py
+++ HancurkanCandi.py
@@ -5,23 +5,29 @@ arrCandi = [0 for i in range (103)] # arrCandi array berisi kumpulan candi, asum
 arrCandi[0] = 1 # hanya sebagai test case, tidak akan dimasukkan di versi akhir
 
 jumlahCandi = length(arrCandi) # sebagai variabel panjang array candi
+useraktif = "Jonggrang" # test case jika user jonggrang
+
 def HancurkanCandi(arrCandi):
-    id = int(input("Masukkan ID candi: "))
-    for i in range(jumlahCandi):
-        if (arrCandi[i] == id):
-            print(f"Apakah anda ingin menghancurkan candi ID: {id} (Y/N)?")
-            jawab = input()
-            if (jawab == "Y") or (jawab == "y"):
-                arrCandi[i] = float('inf')
-                print("Candi telah berhasil dihancurkan.")
-                break
-            elif (jawab == "N") or (jawab == "n"):
-                print("Candi tidak jadi dihancurkan.")
-                break
-        else:
-            if (i == jumlahCandi-1):
-                print("Tidak ada candi dengan ID tersebut.")
-                break
+    if (useraktif == "Jonggrang"): 
+        id = int(input("Masukkan ID candi: "))
+        for i in range(jumlahCandi):
+            if (arrCandi[i] == id):
+                print(f"Apakah anda ingin menghancurkan candi ID: {id} (Y/N)?")
+                jawab = input()
+                if (jawab == "Y") or (jawab == "y"):
+                    arrCandi[i] = float('inf')
+                    print("Candi telah berhasil dihancurkan.")
+                    break
+                elif (jawab == "N") or (jawab == "n"):
+                    print("Candi tidak jadi dihancurkan.")
+                    break
+            else:
+                if (i == jumlahCandi-1):
+                    print("Tidak ada candi dengan ID tersebut.")
+                    break
+    else:
+        print("Anda tidak memiliki akses untuk fungsi tersebut!") # output jika user lain mencoba memakai fungsi tersebut
+
 
 HancurkanCandi(arrCandi) #test case
 
