a6ef081e9556f8fad08e1c58e57d5f221f140e04
Reasondro <116406357+Reasondro@users.noreply.github.com>
2023-04-18 21:40:36 +0700

    Update 1 Berkokok

--- AyamBerkokok.py
+++ AyamBerkokok.py
@@ -1,16 +1,20 @@
 from ProgramUtama  import length
-# arrCandi array berisi kumpulan candi
-jumlahCandi = len(arrCandi)
-def ayam_berkokok (jumlahcandi):
+arrCandi = [0 for i in range (103)] # arrCandi array berisi kumpulan candi, asumsi sudah penuh
+
+jumlahCandi = length(arrCandi)
+def ayam_berkokok (arrCandi):
     if (jumlahCandi) < (100):
         print("Kukuruyuk.. Kukuruyuk.. \n")
         print(f"Jumlah Candi: {jumlahCandi} \n ")
         print("Selamat, Roro Jonggrang memenangkan permainan! \n")
         print("*Bandung Bondowoso angry noise*")
         print("Roro Jonggrang dikutuk menjadi candi.")
+        exit()
     else:
         print("Kukuruyuk.. Kukuruyuk.. \n")
         print(f"Jumlah Candi: {jumlahCandi} \n ")
         print("Yah, Bandung Bondowoso memenangkan permainan!")
-        
+        exit()
 
+
+ayam_berkokok(arrCandi) # test case jika array sudah 100 atau lebih
