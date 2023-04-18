bd348f5d8c08e7b6a3ae7c7adb0eed26f9c1515f
Reasondro <116406357+Reasondro@users.noreply.github.com>
2023-04-18 21:47:14 +0700

    Update 2 AyamBerkokok

--- AyamBerkokok.py
+++ AyamBerkokok.py
@@ -1,20 +1,26 @@
 from ProgramUtama  import length
 arrCandi = [0 for i in range (103)] # arrCandi array berisi kumpulan candi, asumsi sudah penuh
 
+useraktif = "Jonggrang" # test case jika user jonggrang
+
 jumlahCandi = length(arrCandi)
 def ayam_berkokok (arrCandi):
-    if (jumlahCandi) < (100):
-        print("Kukuruyuk.. Kukuruyuk.. \n")
-        print(f"Jumlah Candi: {jumlahCandi} \n ")
-        print("Selamat, Roro Jonggrang memenangkan permainan! \n")
-        print("*Bandung Bondowoso angry noise*")
-        print("Roro Jonggrang dikutuk menjadi candi.")
-        exit()
+    if (useraktif == "Jonggrang"):
+        if (jumlahCandi) < (100):
+            print("Kukuruyuk.. Kukuruyuk.. \n")
+            print(f"Jumlah Candi: {jumlahCandi} \n ")
+            print("Selamat, Roro Jonggrang memenangkan permainan! \n")
+            print("*Bandung Bondowoso angry noise*")
+            print("Roro Jonggrang dikutuk menjadi candi.")
+            exit()
+        else:
+            print("Kukuruyuk.. Kukuruyuk.. \n")
+            print(f"Jumlah Candi: {jumlahCandi} \n ")
+            print("Yah, Bandung Bondowoso memenangkan permainan!")
+            exit()
     else:
-        print("Kukuruyuk.. Kukuruyuk.. \n")
-        print(f"Jumlah Candi: {jumlahCandi} \n ")
-        print("Yah, Bandung Bondowoso memenangkan permainan!")
-        exit()
+        print("Anda tidak memiliki akses untuk fungsi tersebut!") # output jika user lain mencoba memakai fungsi tersebut
+
 
 
 ayam_berkokok(arrCandi) # test case jika array sudah 100 atau lebih
