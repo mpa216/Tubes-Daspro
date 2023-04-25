66ee06777253f668c8b52501ee29cc6149c52e51
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-25 19:52:28 +0700

    Update ProgramUtama.py

--- ProgramUtama.py
+++ ProgramUtama.py
@@ -34,3 +34,23 @@ def del_element(arr, index):
         if i != index:
             new_arr.append(arr[i])
     return new_arr
+
+def my_max(seq):
+    if len(seq) == 0:
+        raise ValueError("max() arg is an empty sequence")
+    else:
+        max_val = seq[0]
+        for val in range(len(seq)):
+            if seq[val] > max_val:
+                max_val = seq[val]
+        return max_val
+
+def my_min(seq):
+    if len(seq) == 0:
+        raise ValueError("min() arg is an empty sequence")
+    else:
+        min_val = seq[0]
+        for val in range(len(seq)):
+            if seq[val] < min_val:
+                min_val = seq[val]
+        return min_val
