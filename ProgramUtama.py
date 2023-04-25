ca4e9a2663a1a6ff1a2ab417da281736300c173b
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-25 08:34:39 +0700

    Update ProgramUtama.py

--- ProgramUtama.py
+++ ProgramUtama.py
@@ -10,3 +10,20 @@ def mappend(arr, c):
         new_arr[i] = arr[i]
     new_arr[length(arr)] = c
     return new_arr
+
+def my_split(string, delimiter=None):
+    result = []
+    current_word = ""
+    if delimiter is None:
+        delimiter = " "
+    for i in range(len(string)):
+        char = string[i]
+        if char == delimiter:
+            if current_word:
+                result = mappend(result, current_word)
+                current_word = ""
+        else:
+            current_word += char
+        if i == len(string) - 1:
+            result = mappend(result, current_word)
+    return result
