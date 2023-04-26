6cec3f1108fefbdf8463057331093eeea177c2b8
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-26 14:39:37 +0700

    Update bangun.py

--- bangun.py
+++ bangun.py
@@ -1,46 +1,50 @@
 import random
+import FungsiUtama
 
-# Semua user di akses dengan indeks, misal index 3 adalah jin ifrit, maka seluruh variable dengan indeks 3 adalah milik jin ifrit. 
-# misal passwordjin[3], jenisjin[3], IDcanditiapjin[3]. semua adalah properti dari jin ifrit
-usernamejin = [] # array ini isinya string ini tempat penyimpanan user name jin,
-passwordjin = [] # array ini isinya string
-jenisjin = [] # 0: pengumpul 1: pembangun, array ini isinya integer
-usernameselainlain = ["Bandung", "Roro"]
-passwordselainlain = ["Bondowoso", "Jonggrang"]
-IDcanditiapjin = [] # Ini array 2 dimensi ex. [[12, 31, 32], [2, 1], [100], ...] misal isi string [12, 31, 32] menandakan IDcandi yg dibuat oleh jin dengan index [0], 
-# ini berfungsi untuk mentrack jumlah idcandi yg dibuat oelh tiap jin, mengingat bahwa satu jin dapat membuat lebih dari 1 Candi
-IDcanditotal = [i for i in range(1, 101)] # Ini array 1 dimensi, yg merupakan versi simple dari IDcanditiapjin, berfungsi untuk menyimpan data idcandi yg belum dibuat
-IDcandidibuat = [] # ini array 1 deimsi, yg menyimpan data mengenai IDcandi yg sudah taken/dibuat
-bahancanditiapjin = [] # Ini array 2 dimensi ex. [[120, 200, 300], [50, 20 , 200] ...] isinya integer dengan format [[pasir, air, batu]]
-totalbahan = [] # ini array 1 dimensi dengan formar [pasir, air, batu]
-useraktif = "" # ini berisi username dari pengguna yg aktif, if useraktif == Bandung : ...... elif useraktif == Roro : ....... else #jin : ......
+useraktif = "jin ifrit" # Musti diisin dengan user yg sedang aktif, nama jin ifrit disini hanya contoh
 
-def length(x):
-    i, sum=0,0
-    while i!=103:
-        if x[i]!=str('inf'): sum+=1
-        i+=1
-    return sum
-
-def mappend(arr, c):
-    new_arr = [None] * (length(arr) + 1)
-    for i in range(length(arr)):
-        new_arr[i] = arr[i]
-    new_arr[length(arr)] = c
-    return new_arr
-
-def bangun(totalbahan):
+def bangun():
+    ids = extract(0, "candi.csv")
+    bahans = extract(2, "bahan_bangunan.csv") # 0 = pasir 1 = batu 2 = air
+    hapus = extract(0, "hapus.csv")
+    for i in range(len(hapus)) :
+        hapus[i] = int(hapus[i])
+    hapussort = my_sort(hapus)
     pasir = random.randint(1, 5)
     batu = random.randint(1, 5)
     air = random.randint(1, 5)
-    if totalbahan[0] >= pasir and totalbahan[1] >= batu and totalbahan[2] >= air :
-        if 100 - length(IDcanditotal) == 0 :           
+
+    pasirreal = int(bahans[0])
+    batureal = int(bahans[1])
+    airreal = int(bahans[2])
+
+    if pasirreal >= pasir and batureal >= batu and airreal >= air :
+        update_bahan((pasirreal - pasir), (batureal - batu), (airreal - air))
+        if 100 - len(ids) - 1 == 0 :           
             print("Candi berhasil dibangun.")
             print("Sisa candi yang perlu dibangun: " + str(0))
         else :
-            # lanjutin . . . . pakai min() untuk nge append idcanditotal
+            if len(ids) == 0 :
+                ygdibuat = 0
+            elif len(hapussort) == 0 :
+                ygdibuat = (int(my_max(ids)) + 1)
+            else :
+                ygdibuat = int(hapussort[0])
+                hapussort = del_element(hapussort, 0)
+                update_hapus(hapussort)
+
+            
+            id = str(ygdibuat)
+            pembuat = str(useraktif)
+            pasir = str(pasir)
+            batu = str(batu)
+            air = str(air)
+            file = open('candi.csv', 'a')
+            file.write("\n" + id + "," + pembuat + "," + pasir + "," + batu + "," + air)
+
             print("Candi berhasil dibangun.")
-            print("Sisa candi yang perlu dibangun: " + str(100 - length(IDcanditotal)))
+            print("Sisa candi yang perlu dibangun: " + str(100 - len(ids) - 1))            
+
     else :
         print("Bahan bangunan tidak mencukupi.")
         print("Candi tidak bisa dibangun!")
