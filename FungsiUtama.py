6df8d1d7b65c319410e4db3bf3d7ac591efa9705
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-26 17:55:40 +0700

    Update FungsiUtama.py

--- FungsiUtama.py
+++ FungsiUtama.py
@@ -1,5 +1,12 @@
 import random
 
+def length(x):
+    i, sum=0,0
+    while i!=103:
+        if x[i]!=str('inf'): sum+=1
+        i+=1
+    return sum
+
 def mappend(arr, c):
     new_arr = [None] * (len(arr) + 1)
     for i in range(len(arr)):
