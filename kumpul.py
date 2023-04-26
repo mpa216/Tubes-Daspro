d66a7091abc8f2c0d2de1f095ea6f9f57f443cb3
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-26 16:38:10 +0700

    Update kumpul.py

--- kumpul.py
+++ kumpul.py
@@ -1,2 +1,16 @@
 import random
 import FungsiUtama
+
+def kumpul():
+    pasirkumpul = random.randint(0, 5)
+    batukumpul = random.randint(0, 5)
+    airkumpul = random.randint(0, 5)
+    bahansebelumkumpul = extract(2, "bahan_bangunan.csv")
+    pasirsebelum = int(bahansebelumkumpul[0])
+    batusebelum = int(bahansebelumkumpul[1])
+    airsebelum = int(bahansebelumkumpul[2])
+    pasirafter = pasirsebelum + pasirkumpul
+    batuafter = batusebelum + batukumpul
+    airafter = airsebelum + airkumpul
+    update_bahan(pasirafter, batuafter, airafter)
+    print(f"Jin Menemukan {pasirkumpul} pasir, {batukumpul} batu, dan {airkumpul} air.")
