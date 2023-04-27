dea0137f8dc8166ad0992bdd41176086fbe3b4c8
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-27 08:15:50 +0700

    Update standardvariable.py

--- standardvariable.py
+++ standardvariable.py
@@ -1,5 +1,5 @@
                                                           # VERSI        ROMBAK #
-arrUser     = [[0 for cols in range (3)] for rows in range(102)] # array 2 dimensi (102 x 3) dengan index 0-99 milik para jin, 100 dimiilki Bandung, 101 dimiliki Roro
+arrUser     = [[str("inf") for cols in range (3)] for rows in range(102)] # array 2 dimensi (102 x 3) dengan index 0-99 milik para jin, 100 dimiilki Bandung, 101 dimiliki Roro
 # arrUser[x][0], arrUser[x][1], arrUser[x][2] = Username,Role, Password
 # Contoh [["Si Cakep","Jin Pembangun","Saya cakep"], ..... , ["Bandung","Antagonist", "Bondowoso"] ,["Roro","Protagonist", "Jonggrang"]]
 userAktif = "" # ini berisi username dari pengguna yg aktif, if useraktif == Bandung : ...... elif useraktif == Roro : ....... else #jin : ......
@@ -12,10 +12,10 @@ usernameJinPengumpul = [] # array 1 dimensi yang berisi nama-nama jin pengumpul 
 # Contoh ["Si Kuat", "Si Gigih", ....]
 passwordJinPengumpul = [] # array 1 dimensi yang berisi password-password jin pengumpul yang sudah ditandai dengan index
 # Contoh ["Saya kuat", "Saya gigih", ....]
-arrCandi    = [[0 for cols in range (5)] for rows in range(100)] # array 2 dimensi (100 x 5) dengan 
+arrCandi    = [[str("inf") for cols in range (5)] for rows in range(100)] # array 2 dimensi (100 x 5) dengan 
 # arrCandi[x][0] = id candi, arrCandi[x][1] = Nama jin pembangunnya, arrCandi[x][2] = jumlah pasir/candi, arrCandi[x][3] = jumlah batu/candi, arrCandi[x][4] = jumlah air/candi
 # Contoh [[1,"Si Cakep",5,3,4],[2,"Si Hebat",4,3,5], ....]
-arrBahan    = [[0 for cols in range (3)] for rows in range(3)] # array 2 dimensi (3 x 3) dengan index 0,1,2 menunjukkan jumlah pasir,batu, air secara berturut
+arrBahan    = [[str("inf") for cols in range (3)] for rows in range(3)] # array 2 dimensi (3 x 3) dengan index 0,1,2 menunjukkan jumlah pasir,batu, air secara berturut
                     # arrBahan[0][0], arrBahan[0][1], arrBahan[0][2] = "Pasir", "Blank", jumlah
 # Maksud deskripsi  # arrBahan[1][0], arrBahan[1][1], arrBahan[1][2] = "Batu", "Blank", jumlah
                     # arrBahan[2][0], arrBahan[2][1],arrBahan[2][2] = "Air", "Blank", jumlah
