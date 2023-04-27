31662f93b78850ef5596a4a518e61bb3c67cf44f
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-27 18:28:23 +0700

    Update FungsiLogin.py

--- FungsiLogin.py
+++ FungsiLogin.py
@@ -1,15 +1,11 @@
-import csv
-def login(nama_file,useraktif):
-    with open(f'{nama_file}',encoding="utf-8") as user:
-        file = csv.reader(user, delimiter=";")
-        gabungan_data = [next(file) for i in range(3)]
+def login(arruser,useraktif):
         username_login, password_login = str(input("Username: ")), str(input("Password: "))
         while useraktif=="":
             for i in range (1,3):
-                if gabungan_data[i][0]==username_login:
-                    if gabungan_data[i][1]==password_login:
+                if arruser[i][0]==username_login:
+                    if arruser[i][1]==password_login:
                         print(f'Selamat datang, {username_login}!\nMasukkan command “help” untuk daftar command yang dapat kamu panggil.')
-                        return username_login
+                        return i
                     else:
                         kondisi = ("Password salah!")
                         break
