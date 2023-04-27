c6d918fc48eb90c5fea53fb2af4a51264db17f66
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-27 18:16:10 +0700

    Update FungsiLogin.py

--- FungsiLogin.py
+++ FungsiLogin.py
@@ -1,19 +1,15 @@
 import csv
-with open("LoginTerakhir.csv",'r') as file:
-    file, loginterakhir = csv.reader(file, delimiter=","), [next(file) for i in range(2)]
-def login(nama_file,udhlogin):
+def login(nama_file,useraktif):
     with open(f'{nama_file}',encoding="utf-8") as user:
         file = csv.reader(user, delimiter=";")
         gabungan_data = [next(file) for i in range(3)]
         username_login, password_login = str(input("Username: ")), str(input("Password: "))
-        while not udhlogin:
+        while useraktif=="":
             for i in range (1,3):
                 if gabungan_data[i][0]==username_login:
                     if gabungan_data[i][1]==password_login:
-                        udhlogin = True
                         print(f'Selamat datang, {username_login}!\nMasukkan command “help” untuk daftar command yang dapat kamu panggil.')
-                        with open("LoginTerakhir.csv",'w',encoding='utf-8') as datalogin:
-                            datalogin.write(f'{username_login},{udhlogin}')
+                        return username_login
                     else:
                         kondisi = ("Password salah!")
                         break
@@ -21,5 +17,5 @@ def login(nama_file,udhlogin):
                     kondisi =("Username tidak terdaftar!")
             print(kondisi)
             username_login, password_login = str(input("Username: ")), str(input("Password: "))
-        if udhlogin:
-            print(f"Anda telah login dengan username {loginterakhir[0][0]}, silahkan lakukan “logout” sebelum melakukan login kembali.")
+        if useraktif!="":
+            print(f"Anda telah login dengan username {useraktif}, silahkan lakukan “logout” sebelum melakukan login kembali.  ")
