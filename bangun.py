889fcc5fbc4cc63ff3b9054200ee0284212f0cac
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-27 09:47:06 +0700

    Update bangun.py

--- bangun.py
+++ bangun.py
@@ -1,52 +1,74 @@
 import random
 import FungsiUtama
 
-IDcandihapus = []
+arrCandi    = [[str("inf") for cols in range (5)] for rows in range(100)] # array 2 dimensi (100 x 5) dengan 
+# arrCandi[x][0] = id candi, arrCandi[x][1] = Nama jin pembangunnya, arrCandi[x][2] = jumlah pasir/candi, arrCandi[x][3] = jumlah batu/candi, arrCandi[x][4] = jumlah air/candi
+# Contoh [[1,"Si Cakep",5,3,4],[2,"Si Hebat",4,3,5], ....]
+arrBahan    = [[str("inf") for cols in range (3)] for rows in range(3)] # array 2 dimensi (3 x 3) dengan index 0,1,2 menunjukkan jumlah pasir,batu, air secara berturut
+                    # arrBahan[0][0], arrBahan[0][1], arrBahan[0][2] = "Pasir", "Blank", jumlah
+# Maksud deskripsi  # arrBahan[1][0], arrBahan[1][1], arrBahan[1][2] = "Batu", "Blank", jumlah
+                    # arrBahan[2][0], arrBahan[2][1],arrBahan[2][2] = "Air", "Blank", jumlah
+# Contoh [["Pasir","Blank", 40], ["Batu","Blank","30"],["Air","Blank","50"]]
+arrBahan = [['pasir', 'blank', 'inf'], ['batu', 'blank', 'inf'], ['air', 'blank', 'inf']]
+# arrUser     = [[str("inf") for cols in range (3)] for rows in range(102)] # array 2 dimensi (102 x 3) dengan index 0-99 milik para jin, 100 dimiilki Bandung, 101 dimiliki Roro
+# arrUser[x][0], arrUser[x][1], arrUser[x][2] = Username,Role, Password
+# Contoh [["Si Cakep","Jin Pembangun","Saya cakep"], ..... , ["Bandung","Antagonist", "Bondowoso"] ,["Roro","Protagonist", "Jonggrang"]]
+userAktif = "" # ini berisi username dari pengguna yg aktif, if useraktif == Bandung : ...... elif useraktif == Roro : ....... else #jin : ......
 
-def bangun(useraktif):
-    # !!! pastikan file candi.csv bebas whitespace demi menghindari kesalahan output !!!
-    ids = extract(0, "candi.csv")
-    bahans = extract(2, "bahan_bangunan.csv") # 0 = pasir 1 = batu 2 = air
-    #hapus = extract(0, "hapus.csv")
-    #for i in range(len(hapus)) :
-    #    hapus[i] = int(hapus[i])
-    #hapussort = my_sort(hapus)
-    hapussort = my_sort(IDcandihapus)
+def bangun(userAktiflokal):
+    global arrBahan
+    global arrCandi 
+
     pasir = random.randint(1, 5)
     batu = random.randint(1, 5)
     air = random.randint(1, 5)
 
-    pasirreal = int(bahans[0])
-    batureal = int(bahans[1])
-    airreal = int(bahans[2])
-
+    if arrBahan[0][2] == "inf" : 
+        print("Bahan bangunan tidak mencukupi.")
+        print("Candi tidak bisa dibangun!")
+        return
+    else :
+        pasirreal = int(arrBahan[0][2])
+        batureal = int(arrBahan[1][2])
+        airreal = int(arrBahan[2][2])
+    
     if pasirreal >= pasir and batureal >= batu and airreal >= air :
-        update_bahan((pasirreal - pasir), (batureal - batu), (airreal - air))
-        if 100 - len(ids) - 1 == 0 :           
-            print("Candi berhasil dibangun.")
-            print("Sisa candi yang perlu dibangun: " + str(0))
-        else :
-            if len(ids) == 0 :
-                ygdibuat = 0
-            elif len(hapussort) == 0 :
-                ygdibuat = (int(my_max(ids)) + 1)
-            else :
-                ygdibuat = int(hapussort[0])
-                hapussort = del_element(hapussort, 0)
-                #update_hapus(hapussort)
-
-            
-            id = str(ygdibuat)
-            pembuat = str(useraktif)
-            pasir = str(pasir)
-            batu = str(batu)
-            air = str(air)
-            file = open('candi.csv', 'a')
-            file.write("\n" + id + "," + pembuat + "," + pasir + "," + batu + "," + air)
-
-            print("Candi berhasil dibangun.")
-            print("Sisa candi yang perlu dibangun: " + str(100 - len(ids) - 1))            
-
+        arrBahan[0][2] = str(pasirreal - pasir)
+        arrBahan[1][2] = str(batureal - batu)
+        arrBahan[2][2] = str(airreal - air) 
+        for i in range(100) :
+            if arrCandi[i][0] == "inf" :
+                arrCandi[i][0] = str(i)
+                arrCandi[i][1] = str(userAktiflokal)
+                arrCandi[i][2] = str(pasir)
+                arrCandi[i][3] = str(batu)
+                arrCandi[i][4] = str(air)
+                print("Candi berhasil dibangun.")
+                print(f"Sisa candi yang perlu dibangun: {100-lengthcandi(arrCandi)}.")
+                return
     else :
         print("Bahan bangunan tidak mencukupi.")
         print("Candi tidak bisa dibangun!")
+        return
+
+
+# Untuk testing
+# userAktif = "dhika"
+# arrBahan[0][2] = 100
+# arrBahan[1][2] = 100
+# arrBahan[2][2] = 100
+# arrCandi[1] = ['1', 'dhika', '2', '3', '4']
+# arrCandi[3] = ['3', 'dhika', '4', '3', '2']
+# gaming = True
+# count = 5
+# while True :
+#     play = input("command?")   
+#     if play == "bangun" :           
+#         bangun(userAktif)
+#         for i in range(0, count+1) :
+#             print(arrCandi[i])
+#         print(arrBahan)
+#         count += 1
+
+#     else :
+#         gaming = False
