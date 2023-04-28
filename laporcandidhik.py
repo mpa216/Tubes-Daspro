7302030249ecdd40b8f5e4e8ee77f5fbbd4107b8
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-28 13:35:36 +0700

    Update laporcandidhik.py

--- laporcandidhik.py
+++ laporcandidhik.py
@@ -1,45 +1,68 @@
 import FungsiUtama
-import standardvariables
 
-totalCandi = FungsiUtama.lengthcandi(standardvariables.arrCandi)
+arrUser     = [[str("inf") for cols in range (3)] for rows in range(102)] # array 2 dimensi (102 x 3) dengan index 0 dimiliki Bandung, 1 dimiilki Roro, dan 2-101 milik para jin
+# arrUser[x][0], arrUser[x][1], arrUser[x][2] = Username, Password, Role
+# Contoh [["Si Cakep","Jin Pembangun","Saya cakep"], ..... , ["Bandung","Antagonist", "Bondowoso"] ,["Roro","Protagonist", "Jonggrang"]]
+userAktif = "" # ini berisi username dari pengguna yg aktif, if useraktif == Bandung : ...... elif useraktif == Roro : ....... else #jin : ......
+roleAktif = "" # ini berisi role dari pengguna yg aktif
+jenisJin = [] # 0: pengumpul 1: pembangun, array ini isinya integer
+usernameJinPembangun = [] # array 1 dimensi yang berisi nama-nama jin pembangun yang sudah ditandai dengan index
+# Contoh ["Si Cakep", "Si Hebat", ....]
+passwordJinPembangun = [] # array 1 dimensi yang berisi password-password jin pembangun yang sudah ditandai dengan index
+# Contoh ["Saya cakep", "Saya hebat", ....]
+usernameJinPengumpul = [] # array 1 dimensi yang berisi nama-nama jin pengumpul yang sudah ditandai dengan index
+# Contoh ["Si Kuat", "Si Gigih", ....]
+passwordJinPengumpul = [] # array 1 dimensi yang berisi password-password jin pengumpul yang sudah ditandai dengan index
+# Contoh ["Saya kuat", "Saya gigih", ....]
+arrCandi    = [[str("inf") for cols in range (5)] for rows in range(100)] # array 2 dimensi (100 x 5) dengan 
+# arrCandi[x][0] = id candi, arrCandi[x][1] = Nama jin pembangunnya, arrCandi[x][2] = jumlah pasir/candi, arrCandi[x][3] = jumlah batu/candi, arrCandi[x][4] = jumlah air/candi
+# Contoh [[1,"Si Cakep",5,3,4],[2,"Si Hebat",4,3,5], ....]
+arrBahan = [['pasir', 'blank', 'inf'], ['batu', 'blank', 'inf'], ['air', 'blank', 'inf']]
+#arrBahan    = [[str("inf") for cols in range (3)] for rows in range(3)] # array 2 dimensi (3 x 3) dengan index 0,1,2 menunjukkan jumlah pasir,batu, air secara berturut
+                    # arrBahan[0][0], arrBahan[0][1], arrBahan[0][2] = "Pasir", "Blank", jumlah
+# Maksud deskripsi  # arrBahan[1][0], arrBahan[1][1], arrBahan[1][2] = "Batu", "Blank", jumlah
+                    # arrBahan[2][0], arrBahan[2][1],arrBahan[2][2] = "Air", "Blank", jumlah
+# Contoh [["Pasir","Blank", 40], ["Batu","Blank","30"],["Air","Blank","50"]]
 
-def hitungPasir(arrCandi):
-    global usedPasir 
+totalCandi = FungsiUtama.lengthcandi(arrCandi)
+
+def hitungPasir():
+    global usedPasir
+    global arrCandi 
     usedPasir = 0
-    arrCandi = standardvariables.arrCandi
     for i in range(100):
         if arrCandi[i][2] != str("inf"):
             usedPasir += int(arrCandi[i][2])
         
 
-def hitungBatu(arrCandi):
+def hitungBatu():
     global usedBatu 
+    global arrCandi
     usedBatu = 0
-    arrCandi = standardvariables.arrCandi
     for i in range(100):
         if arrCandi[i][3] != str("inf"):
             usedBatu += int(arrCandi[i][3])
    
-def hitungAir(arrCandi):
+def hitungAir():
     global usedAir 
+    global arrCandi
     usedAir = 0
-    arrCandi = standardvariables.arrCandi
     for i in range(100):
         if arrCandi[i][4] != str("inf"):
             usedAir += int(arrCandi[i][4])
 
-hitungPasir(standardvariables.arrCandi)
-hitungBatu(standardvariables.arrCandi)
-hitungAir(standardvariables.arrCandi)
+hitungPasir()
+hitungBatu()
+hitungAir()
 
-def hargaCandi(arrCandi):
+def hargaCandi():
+    global arrCandi
     global candiAda
     global tajMahal
     global biayaTajMahal
     global tajMurah
     global biayaTajMurah
     candiAda = [[0 for cols in range(2)] for rows in range(101)]
-    arrCandi = standardvariables.arrCandi
     for i in range(totalCandi):
         if (arrCandi[i][0] != str("inf")):
             candiAda[i][0] = arrCandi[i][0]
@@ -59,7 +82,7 @@ def hargaCandi(arrCandi):
                 biayaTajMurah = candiAda[i][1]           
 
             
-hargaCandi(standardvariables.arrCandi)
+hargaCandi()
 
 def laporanCandi():
     print(f"Total Candi: {totalCandi}")
