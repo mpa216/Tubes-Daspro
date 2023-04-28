df1cbb0e4055b436f5f3faa8a3a35d1460effbd8
Reasondro <116406357+Reasondro@users.noreply.github.com>
2023-04-28 12:08:25 +0700

    versi 1

---
+++ ambilLaporanCandi.py
@@ -0,0 +1,75 @@
+import FungsiUtama
+import standardvariables
+
+totalCandi = FungsiUtama.lengthcandi(standardvariables.arrCandi)
+
+def hitungPasir(arrCandi):
+    global usedPasir 
+    usedPasir = 0
+    arrCandi = standardvariables.arrCandi
+    for i in range(100):
+        if arrCandi[i][2] != str("inf"):
+            usedPasir += int(arrCandi[i][2])
+    return usedPasir
+        
+
+def hitungBatu(arrCandi):
+    global usedBatu 
+    usedBatu = 0
+    arrCandi = standardvariables.arrCandi
+    for i in range(100):
+        if arrCandi[i][3] != str("inf"):
+            usedBatu += int(arrCandi[i][3])
+    return usedBatu
+   
+def hitungAir(arrCandi):
+    global usedAir 
+    usedAir = 0
+    arrCandi = standardvariables.arrCandi
+    for i in range(100):
+        if arrCandi[i][4] != str("inf"):
+            usedAir += int(arrCandi[i][4])
+    return usedAir
+
+hitungPasir(standardvariables.arrCandi)
+hitungBatu(standardvariables.arrCandi)
+hitungAir(standardvariables.arrCandi)
+
+def hargaCandi(arrCandi):
+    global candiAda
+    global tajMahal
+    global biayaTajMahal
+    global tajMurah
+    global biayaTajMurah
+    candiAda = [[0 for cols in range(2)] for rows in range(101)]
+    arrCandi = standardvariables.arrCandi
+    for i in range(totalCandi):
+        if (arrCandi[i][0] != str("inf")):
+            candiAda[i][0] = arrCandi[i][0]
+            candiAda[i][1] = (int(arrCandi[i][2])*10000) + (int(arrCandi[i][3])*15000) + (int(arrCandi[i][4])*7500)
+    tajMahal = candiAda[0][0]
+    biayaTajMahal = float("-inf")      
+    for i in range(100):
+        if candiAda[i][1] > biayaTajMahal:
+            tajMahal = candiAda[i][0]
+            biayaTajMahal = candiAda[i][1]
+    tajMurah = candiAda[0][0]
+    biayaTajMurah = float("inf")
+    for i in range(100):
+        if (candiAda[i][1] != 0):
+            if candiAda[i][1] < biayaTajMurah:
+                tajMurah = candiAda[i][0]
+                biayaTajMurah = candiAda[i][1]           
+
+            
+hargaCandi(standardvariables.arrCandi)
+
+def laporanCandi():
+    print(f"Total Candi: {totalCandi}")
+    print(f"Total Pasir yang digunakan: {usedPasir}")
+    print(f"Total Batu yang digunakan: {usedBatu}")
+    print(f"Total Air yang digunakan: {usedAir}")
+    print(f"ID Candi Termahal: {tajMahal} (Rp {biayaTajMahal})")
+    print(f"ID Candi Termurah: {tajMurah} (Rp {biayaTajMurah})")
+
+# laporanCandi()
