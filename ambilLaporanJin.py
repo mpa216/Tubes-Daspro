f8bc2300715ff8ce0752e4d3652f6074169a085b
Reasondro <116406357+Reasondro@users.noreply.github.com>
2023-04-28 10:18:06 +0700

    versi tanpa jin rajin/malas (ongoing)
    ini jin rajin/malas ny lgi dibuat, rada kesulitan

---
+++ ambilLaporanJin.py
@@ -0,0 +1,67 @@
+import FungsiUtama
+import standardvariables
+
+def hitungJin(arrUser):
+    global totalJin
+    arrUser = standardvariables.arrUser
+    totalJin = 0
+    for i in range(102):
+        if ((arrUser[i][2] == "Jin Pembangun")  or (arrUser[i][2] == "Jin Pengumpul")):
+            totalJin += 1
+    return totalJin
+
+def hitungJinPengumpul(arrUser):
+    global totalJinPengumpul
+    arrUser = standardvariables.arrUser
+    totalJinPengumpul = 0
+    for i in range(102):
+        if (arrUser[i][2] == 'Jin Pengumpul'):
+            totalJinPengumpul += 1
+    return totalJinPengumpul
+
+def hitungJinPembangun(arrUser):
+    global totalJinPembangun
+    arrUser = standardvariables.arrUser
+    totalJinPembangun = 0
+    for i in range(102):
+        if (arrUser[i][2] == "Jin Pembangun"):
+            totalJinPembangun += 1
+    return totalJinPembangun
+
+pasir = standardvariables.arrBahan[0][2]  
+air = standardvariables.arrBahan[2][2]    
+batu = standardvariables.arrBahan[1][2]    
+ 
+if (pasir == "inf"):
+    pasir = "0"
+if (air == "inf"):
+    air = "0"
+if (batu == "inf"):
+    batu = "0"
+    
+hitungJin(standardvariables.arrUser)
+hitungJinPengumpul(standardvariables.usernameJinPengumpul)
+hitungJinPembangun(standardvariables.arrUser)
+
+def laporanJin():
+    print(f"Total Jin: {totalJin} ")
+    print(f"Total Jin Pengumpul: {totalJinPengumpul}")
+    print(f"Total Jin Pembangun: {totalJinPembangun} ")
+    print(f"Jin Terajin:  ")
+    print(f"Jin Termalas: ")
+    print(f"Jumlah Pasir:{pasir} ")
+    print(f"Jumlah Air: {air}")
+    print(f"Jumlah Batu: {batu} ")
+
+laporanJin()
+
+
+
+
+
+    
+
+  
+    
+
+
