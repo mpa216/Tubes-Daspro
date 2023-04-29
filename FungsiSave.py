804a1c5cf740c17c0cbf9d076eebcaa2c20ba6c2
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-29 16:34:52 +0700

    Update FungsiSave.py

--- FungsiSave.py
+++ FungsiSave.py
@@ -7,24 +7,52 @@ def save_data(nama_folder):
     #Cek apakah folder ada
     if not os.path.exists(nama_folder):
         print(f"Folder '{nama_folder}' tidak ditemukan.")
+    #Load data dari file-file dalam folder
     else:
         with open(f"{nama_folder}\\user.csv", "w") as file:
-            for row in arrUser:
-                for col in row:
-                    file.write(str(col) + ";")
-                file.write("\n")
+            skip = True
+            header = ["username","password","role"]
+            for col in range (3):
+                file.write(str(header[col]) + ";")
+            file.write("\n")
+            for row in range (103):
+                for col in range (3):
+                    if arrUser[row][col]!='inf':
+                        file.write(str(arrUser[row][col]) + ";")
+                        skip = False
+                if not skip:
+                    file.write("\n")
+                skip=True
             
         with open(f"{nama_folder}\\candi.csv", "w") as file:
-            for row in arrCandi:
-                for col in row:
-                    file.write(str(col) + ";")
-                file.write("\n")
-
+            skip = True
+            header = ["id","pembuat","pasir","batu","air"]
+            for col in range (5):
+                file.write(str(header[col]) + ";")
+            file.write("\n")
+            for row in range (100):
+                for col in range (5):
+                    if arrCandi[row][col]!='inf':
+                        file.write(str(arrCandi[row][col]) + ";")
+                        skip = False
+                if not skip:
+                    file.write("\n")
+                skip=True
+    
         with open(f"{nama_folder}\\bahan_bangunan.csv", "w") as file:
-            for row in arrBahan:
-                for col in row:
-                    file.write(str(col) + ";")
-                file.write("\n")
+            skip = True
+            header = ["nama","deskripsi","jumlah"]
+            for col in range (3):
+                file.write(str(header[col]) + ";")
+            file.write("\n")
+            for row in range (3):
+                for col in range (3):
+                    if arrBahan[row][col]!='inf':
+                        file.write(str(arrBahan[row][col]) + ";")
+                        skip = False
+                if not skip:
+                    file.write("\n")
+                skip=True
     return
 
 if __name__ == '__main__':
