6c348b0ce1721742475f8bfe126b62824e7699ce
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-29 11:44:19 +0700

    Update logoutdhik.py

--- logoutdhik.py
+++ logoutdhik.py
@@ -1,1 +1,8 @@
+userAktif = ""
 
+def logout():
+    global userAktif
+    if userAktif == "" :
+        print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
+    else :
+        userAktif = ""
