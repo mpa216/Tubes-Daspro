7ba2e5b077059e19eaf47650010b877f456789c1
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-29 12:33:58 +0700

    Update FungsiSummonJin.py

--- FungsiSummonJin.py
+++ FungsiSummonJin.py
@@ -7,28 +7,32 @@ def jenis_jin(arrUser):
         x = int(input("Masukkan nomor jenis jin yang ingin dipanggil: "))
     username_jin = str(input("Masukkan username jin: "))
     password_jin = str(input("Masukkan password jin: "))
-    if x==1:
-        print("Memilih jin “Pengumpul”.")
-        for i in range (2,103):
-            if arrUser[i][0]==username_jin:
-                print(f'Username "{username_jin}" sudah diambil!')
-                username_jin = str(input("Masukkan username jin: "))
-                tambah = False
-                break
-            else:
-                tambah = True
-        if tambah:
-            arrUser = append_lain(arrUser,[username_jin,password_jin,'Pengumpul'])
-    elif x==2:
-        print("Memilih jin Pembangun.")
-        for i in range (2,103):
-            if arrUser[i][0]==username_jin:
-                print(f'Username "{username_jin}" sudah diambil!')
-                username_jin = str(input("Masukkan username jin: "))
-                tambah = False
-                break
-            else:
-                tambah = True
-        if tambah:
-            arrUser = append_lain(arrUser,[username_jin,password_jin,'Pembangun'])
+    if length(arrUser)<103:
+        if x==1:
+            print("Memilih jin “Pengumpul”.")
+            for i in range (2,103):
+                if arrUser[i][0]==username_jin:
+                    print(f'Username "{username_jin}" sudah diambil!')
+                    username_jin = str(input("Masukkan username jin: "))
+                    tambah = False
+                    break
+                else:
+                    tambah = True
+                j+=1
+            if tambah:
+                arrUser = append_lain(arrUser,[username_jin,password_jin,'Pengumpul'])
+        elif x==2:
+            print("Memilih jin Pembangun.")
+            for i in range (2,103):
+                if arrUser[i][0]==username_jin:
+                    print(f'Username "{username_jin}" sudah diambil!')
+                    username_jin = str(input("Masukkan username jin: "))
+                    tambah = False
+                    break
+                else:
+                    tambah = True
+            if tambah:
+                arrUser = append_lain(arrUser,[username_jin,password_jin,'Pembangun'])
+    else:
+        print("Jin sudah Penuh")
     return
