2752694bfb3d3895f07e9a83eecd00ff0624e6a7
mpa216 <123876878+mpa216@users.noreply.github.com>
2023-04-29 12:03:18 +0700

    Update ProgramUtama.py

--- ProgramUtama.py
+++ ProgramUtama.py
@@ -1,1 +1,8 @@
-import FungsiUtama
+from FungsiUtama import length,lengthcandi,mappend,my_split,del_element,my_max,my_min,my_sort
+import random
+import sys
+import argparse
+import csv
+import os
+import random
+
