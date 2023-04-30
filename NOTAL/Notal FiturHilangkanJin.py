54cb473578c1410386943329ecd3fc9fee60cc4d
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-30 21:35:43 +0700

    Add files via upload

---
+++ NOTAL/Notal FiturHilangkanJin.py
@@ -0,0 +1,27 @@
+procedure HilangkanJin():
+  global arrUser
+  global arrCandi
+  input(jin_hilang)
+  kondisi <- False
+  i<-1
+  while (not kondisi) or x<103 do
+    if arrUser[i][1] = jin_hilang then
+      kondisi <- True
+    stop kondisi = True
+  if kondisi then
+    input(hilang)
+    while not (hilang="y" or hilang="Y" or hilang="N" or hilang="n") then
+      output("Input salah.")
+      input(hilang)
+    if  hilang='Y' or hilang='y'then
+      j <- 3
+      repeat 99 times
+        arrUser[j] <- arrUser[j+1]
+      arrUser[102] <- ['inf','inf','inf']
+
+      j <- 1
+      repeat 100 times
+        if arrCandi[j][2]=jin_hilang then
+          arrCandi[j] <- ['inf','inf','inf','inf','inf']
+    else
+      output("Jin tidak jadi dihilangkan.")
