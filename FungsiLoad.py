7e9b6ab2baec6a78db14ebe6608fc6179cca7a35
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-30 11:22:58 +0700

    Update FungsiLoad.py

--- FungsiLoad.py
+++ FungsiLoad.py
@@ -21,14 +21,15 @@ def baca_csv(file_path, delimiter=';'):
     rows = ['inf']
     with open(file_path, 'r') as file:
         line = file.readline()
+        print(line)
         while line:
             row = ['inf']
             cell_value = ''
             dalampetik = False
             for i in range (len(line)):
-                if line[i] == '"' and not dalampetik:
+                if line[i] == "'" and not dalampetik:
                     dalampetik = True
-                elif line[i] == '"' and dalampetik:
+                elif (line[i] == '"') and dalampetik:
                     dalampetik = False
                 elif line[i] == delimiter and not dalampetik:
                     row = mappend_lain_2(row, cell_value)
@@ -39,6 +40,17 @@ def baca_csv(file_path, delimiter=';'):
             rows = mappend_lain_2(rows, row)
             line = file.readline()
     return rows
+
+def hapus_space(line):
+    arr = ['*' for i in range (len(line)-2)]
+    for i in range (len(line)-2):
+        arr[i]=line[i]
+    baris = ''
+    for i in range (len(line)-2):
+        baris = baris+arr[i]
+    print(arr)
+    return baris
+        
 
 def load(nama_folder):
     global arrUser
@@ -56,18 +68,27 @@ def load(nama_folder):
             for i in range (1,len_lain(data_user)):
                 for j in range (3):
                     arrUser[i-1][j]=data_user[i][j]
+                if arrUser[i-1][2]!='inf':
+                    arrUser[i-1][2] = hapus_space(arrUser[i-1][2])
+            print(arrUser)
             
         with open(f'{nama_folder}\\candi.csv', 'r') as file:
             data_candi = baca_csv(file.name, delimiter=";")
             for i in range (1,len_lain(data_candi)):
                 for j in range (5):
                     arrCandi[int(data_candi[i][0])-1][j]=data_candi[i][j]
+                if arrCandi[int(data_candi[i][0])-1][4]!='inf':
+                    arrCandi[int(data_candi[i][0])-1][4] = hapus_space(arrCandi[int(data_candi[i][0])-1][4])
+            print(arrCandi)
 
         with open(f'{nama_folder}\\bahan_bangunan.csv', 'r') as file:
             data_bahan = baca_csv(file.name, delimiter=";")
             for i in range (1,len_lain(data_bahan)):
                 for j in range (3):
                     arrBahan[i-1][j]=data_bahan[i][j]
+                if arrBahan[i-1][2]!='inf':
+                    arrBahan[i-1][2] = hapus_space(arrUser[i-1][2])
+            print(arrBahan)
             return
 
 if __name__ == '__main__':
