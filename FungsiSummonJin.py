9c9e19461c99e7773f131deb04ed5b7e896f0756
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-30 10:17:31 +0700

    Update FungsiSummonJin.py

--- FungsiSummonJin.py
+++ FungsiSummonJin.py
@@ -7,7 +7,7 @@ def jenis_jin(arrUser):
         x = int(input("Masukkan nomor jenis jin yang ingin dipanggil: "))
     username_jin = str(input("Masukkan username jin: "))
     password_jin = str(input("Masukkan password jin: "))
-    if length(arrUser)<103:
+    if length(arrUser)<102:
         if x==1:
             print("Memilih jin “Pengumpul”.")
             for i in range (2,103):
@@ -23,7 +23,7 @@ def jenis_jin(arrUser):
                 arrUser = append_lain(arrUser,[username_jin,password_jin,'Pengumpul'])
         elif x==2:
             print("Memilih jin Pembangun.")
-            for i in range (2,103):
+            for i in range (2,102):
                 if arrUser[i][0]==username_jin:
                     print(f'Username "{username_jin}" sudah diambil!')
                     username_jin = str(input("Masukkan username jin: "))
