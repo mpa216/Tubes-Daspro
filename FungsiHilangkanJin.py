c700d1fe6912d8cd284712dbbd665614073835c6
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-30 10:19:15 +0700

    Update FungsiHilangkanJin.py

--- FungsiHilangkanJin.py
+++ FungsiHilangkanJin.py
@@ -15,11 +15,11 @@ def HilangkanJin():
             hilang = str(input("Apakah anda yakin ingin menghapus jin dengan username Jin1 (Y/N)? "))
         if hilang=='Y' or hilang=='y':
             for j in range (2,101):
-                arrUser[i]=arrUser[i+1]
+                arrUser[j]=arrUser[j+1]
             arrUser[101] = ['inf','inf','inf']
             for j in range (100):
-                if arrCandi[i][1]==jin_hilang:
-                    arrCandi[i]=['inf','inf','inf','inf','inf']
+                if arrCandi[j][1]==jin_hilang:
+                    arrCandi[j]=['inf','inf','inf','inf','inf']
         elif hilang=="N" or hilang=="n":
             print("Jin tidak jadi dihilangkan.")
         
