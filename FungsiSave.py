351f081f3593453c7b1d84d477380788fb20c67e
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-30 13:13:05 +0700

    Update FungsiSave.py

--- FungsiSave.py
+++ FungsiSave.py
@@ -1,66 +1,69 @@
-import argparse
 import os
+arrUser = [['inf' for j in range (3)] for i in range (103)]
+arrUser[1] = ['Bandung', 'cintaroro', 'bandungbondowoso']
+arrUser[5] = ['Bandung', 'simproro', 'bandungbondowoso']
+arrCandi = [['inf' for j in range (5)] for i in range (100)]
+arrBahan = [['inf' for j in range (3)] for i in range (3)]
+
 def tulis_header(header,path):
     with open(path,'w') as f:
         return f.write(f'{header}\n')
     
-def save(nama_folder):
+def save():
     global arrUser
     global arrCandi
     global arrBahan
     #Cek apakah folder ada
-    if not os.path.exists(nama_folder):
-        print(f"Folder '{nama_folder}' tidak ditemukan.")
-    #Load data dari file-file dalam folder
-    else:
-        with open(f"{nama_folder}\\user.csv", "a") as file:
-            skip = True
-            tulis_header("username;password;role",f"{nama_folder}\\user.csv")
-            for row in range (103):
-                for col in range (3):
-                    if arrUser[row][col]!='inf':
-                        if col<2:
-                            file.write(str(arrUser[row][col]) + ";")
-                        else:
-                            file.write(str(arrUser[row][col]))
-                        skip = False
-                if not skip:
-                    file.write("\n")
-                skip=True
-            
-        with open(f"{nama_folder}\\candi.csv", "a") as file:
-            skip = True
-            tulis_header("id;pembuat;pasir;batu;air",f"{nama_folder}\\candi.csv")
-            for row in range (100):
-                for col in range (5):
-                    if arrCandi[row][col]!='inf':
-                        if col<4:
-                            file.write(str(arrCandi[row][col]) + ";")
-                        else:
-                            file.write(str(arrCandi[row][col]))
-                        skip = False
-                if not skip:
-                    file.write("\n")
-                skip=True
-    
-        with open(f"{nama_folder}\\bahan_bangunan.csv", "a") as file:
-            skip = True
-            tulis_header("nama;deskripsi;jumlah",f"{nama_folder}\\bahan_bangunan.csv")
-            for row in range (3):
-                for col in range (3):
-                    if arrBahan[row][col]!='inf':
-                        if col<2:
-                            file.write(str(arrBahan[row][col]) + ";")
-                        else:
-                            file.write(str(arrBahan[row][col]))
-                        skip = False
-                if not skip:
-                    file.write("\n")
-                skip=True
-    return
+    nama_folder = str(input(""))
+    path = f'save\\{nama_folder}'
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Prosedur untuk memuat data dari file-file dalam suatu folder.")
-    parser.add_argument('nama_folder', type=str, help="Nama folder yang berisi file-file penyimpanan.")
-    args = parser.parse_args()
-    save(args.nama_folder)
+    print("Saving...")
+    if not os.path.exists(path):
+        print(f"Membuat folder save/{nama_folder}...")
+        os.mkdir(path)
+    print(f"Berhasil menyimpan data di folder {nama_folder}")
+    with open(f"{path}\\user.csv", "a") as file:
+        skip = True
+        tulis_header("username;password;role",f"{path}\\user.csv")
+        for row in range (103):
+            for col in range (3):
+                if arrUser[row][col]!='inf':
+                    if col<2:
+                        file.write(str(arrUser[row][col]) + ";")
+                    else:
+                        file.write(str(arrUser[row][col]))
+                    skip = False
+            if not skip:
+                file.write("\n")
+            skip=True
+        
+    with open(f"{path}\\candi.csv", "a") as file:
+        skip = True
+        tulis_header("id;pembuat;pasir;batu;air",f"{path}\\candi.csv")
+        for row in range (100):
+            for col in range (5):
+                if arrCandi[row][col]!='inf':
+                    if col<4:
+                        file.write(str(arrCandi[row][col]) + ";")
+                    else:
+                        file.write(str(arrCandi[row][col]))
+                    skip = False
+            if not skip:
+                file.write("\n")
+            skip=True
+
+    with open(f"{path}\\bahan_bangunan.csv", "a") as file:
+        skip = True
+        tulis_header("nama;deskripsi;jumlah",f"{path}\\bahan_bangunan.csv")
+        for row in range (3):
+            for col in range (3):
+                if arrBahan[row][col]!='inf':
+                    if col<2:
+                        file.write(str(arrBahan[row][col]) + ";")
+                    else:
+                        file.write(str(arrBahan[row][col]))
+                    skip = False
+            if not skip:
+                file.write("\n")
+            skip=True
+    return
