283b62c762811933694f37b0e3ceedb12fa3e57a
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-30 10:16:21 +0700

    Update FungsiLogin.py

--- FungsiLogin.py
+++ FungsiLogin.py
@@ -1,14 +1,17 @@
-def login(arruser,useraktif,roleaktif):
+def login():
+        global arrUser
+        global useraktif
+        global roleaktif
         username_login, password_login = str(input("Username: ")), str(input("Password: "))
         if useraktif!="":
             print(f"Anda telah login dengan username {useraktif}, silahkan lakukan “logout” sebelum melakukan login kembali.  ")
         while useraktif=="" and roleaktif=="":
             for i in range (0,2): 
-                if arruser[i][0]==username_login:
-                    if arruser[i][1]==password_login:
+                if arrUser[i][0]==username_login:
+                    if arrUser[i][1]==password_login:
                         kondisi = (f'Selamat datang, {username_login}!\nMasukkan command “help” untuk daftar command yang dapat kamu panggil.')
-                        useraktif=arruser[i][0]
-                        roleaktif=arruser[i][2]
+                        useraktif=arrUser[i][0]
+                        roleaktif=arrUser[i][2]
                         break
                     else:
                         kondisi = ("Password salah!")
