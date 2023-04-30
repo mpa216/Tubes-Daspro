adfa8ff1d83df0e4db5a9746ba92c9784d9e2076
William <115348219+Azekhiel@users.noreply.github.com>
2023-04-30 14:29:13 +0700

    Update ProgramUtama.py

--- ProgramUtama.py
+++ ProgramUtama.py
@@ -4,6 +4,32 @@ import sys
 import argparse
 import os
 
+
+arrUser     = [[str("inf") for cols in range (3)] for rows in range(102)] # array 2 dimensi (102 x 3) dengan index 0 dimiliki Bandung, 1 dimiilki Roro, dan 2-101 milik para jin
+# arrUser[x][0], arrUser[x][1], arrUser[x][2] = Username, Password, Role
+# Contoh [["Si Cakep","Saya cakep","Saya Cakep"], ..... , ["Bandung","Bondowoso","Antagonist"] ,["Roro","Jonggrang","Protagonist"]]
+userAktif = "" # ini berisi username dari pengguna yg aktif, if useraktif == Bandung : ...... elif useraktif == Roro : ....... else #jin : ......
+roleAktif = "" # ini berisi role dari pengguna yg aktif
+jenisJin = [] # 0: pengumpul 1: pembangun, array ini isinya integer
+usernameJinPembangun = [] # array 1 dimensi yang berisi nama-nama jin pembangun yang sudah ditandai dengan index
+# Contoh ["Si Cakep", "Si Hebat", ....]
+passwordJinPembangun = [] # array 1 dimensi yang berisi password-password jin pembangun yang sudah ditandai dengan index
+# Contoh ["Saya cakep", "Saya hebat", ....]
+usernameJinPengumpul = [] # array 1 dimensi yang berisi nama-nama jin pengumpul yang sudah ditandai dengan index
+# Contoh ["Si Kuat", "Si Gigih", ....]
+passwordJinPengumpul = [] # array 1 dimensi yang berisi password-password jin pengumpul yang sudah ditandai dengan index
+# Contoh ["Saya kuat", "Saya gigih", ....]
+arrCandi    = [[str("inf") for cols in range (5)] for rows in range(100)] # array 2 dimensi (100 x 5) dengan 
+# arrCandi[x][0] = id candi, arrCandi[x][1] = Nama jin pembangunnya, arrCandi[x][2] = jumlah pasir/candi, arrCandi[x][3] = jumlah batu/candi, arrCandi[x][4] = jumlah air/candi
+# Contoh [[1,"Si Cakep",5,3,4],[2,"Si Hebat",4,3,5], ....]
+arrBahan = [['pasir', 'blank', 'inf'], ['batu', 'blank', 'inf'], ['air', 'blank', 'inf']]
+#arrBahan    = [[str("inf") for cols in range (3)] for rows in range(3)] # array 2 dimensi (3 x 3) dengan index 0,1,2 menunjukkan jumlah pasir,batu, air secara berturut
+                    # arrBahan[0][0], arrBahan[0][1], arrBahan[0][2] = "Pasir", "Blank", jumlah
+# Maksud deskripsi  # arrBahan[1][0], arrBahan[1][1], arrBahan[1][2] = "Batu", "Blank", jumlah
+                    # arrBahan[2][0], arrBahan[2][1],arrBahan[2][2] = "Air", "Blank", jumlah
+# Contoh [["Pasir","Blank", 40], ["Batu","Blank","30"],["Air","Blank","50"]]
+
+
 def login():
         global arrUser
         global userAktif
@@ -617,34 +643,6 @@ def hilangkanjin():
         print("Tidak ada jin dengan username itu")
 
     return
-
-
-
-
-
-arrUser     = [[str("inf") for cols in range (3)] for rows in range(102)] # array 2 dimensi (102 x 3) dengan index 0 dimiliki Bandung, 1 dimiilki Roro, dan 2-101 milik para jin
-# arrUser[x][0], arrUser[x][1], arrUser[x][2] = Username, Password, Role
-# Contoh [["Si Cakep","Saya cakep","Saya Cakep"], ..... , ["Bandung","Bondowoso","Antagonist"] ,["Roro","Jonggrang","Protagonist"]]
-userAktif = "" # ini berisi username dari pengguna yg aktif, if useraktif == Bandung : ...... elif useraktif == Roro : ....... else #jin : ......
-roleAktif = "" # ini berisi role dari pengguna yg aktif
-jenisJin = [] # 0: pengumpul 1: pembangun, array ini isinya integer
-usernameJinPembangun = [] # array 1 dimensi yang berisi nama-nama jin pembangun yang sudah ditandai dengan index
-# Contoh ["Si Cakep", "Si Hebat", ....]
-passwordJinPembangun = [] # array 1 dimensi yang berisi password-password jin pembangun yang sudah ditandai dengan index
-# Contoh ["Saya cakep", "Saya hebat", ....]
-usernameJinPengumpul = [] # array 1 dimensi yang berisi nama-nama jin pengumpul yang sudah ditandai dengan index
-# Contoh ["Si Kuat", "Si Gigih", ....]
-passwordJinPengumpul = [] # array 1 dimensi yang berisi password-password jin pengumpul yang sudah ditandai dengan index
-# Contoh ["Saya kuat", "Saya gigih", ....]
-arrCandi    = [[str("inf") for cols in range (5)] for rows in range(100)] # array 2 dimensi (100 x 5) dengan 
-# arrCandi[x][0] = id candi, arrCandi[x][1] = Nama jin pembangunnya, arrCandi[x][2] = jumlah pasir/candi, arrCandi[x][3] = jumlah batu/candi, arrCandi[x][4] = jumlah air/candi
-# Contoh [[1,"Si Cakep",5,3,4],[2,"Si Hebat",4,3,5], ....]
-arrBahan = [['pasir', 'blank', 'inf'], ['batu', 'blank', 'inf'], ['air', 'blank', 'inf']]
-#arrBahan    = [[str("inf") for cols in range (3)] for rows in range(3)] # array 2 dimensi (3 x 3) dengan index 0,1,2 menunjukkan jumlah pasir,batu, air secara berturut
-                    # arrBahan[0][0], arrBahan[0][1], arrBahan[0][2] = "Pasir", "Blank", jumlah
-# Maksud deskripsi  # arrBahan[1][0], arrBahan[1][1], arrBahan[1][2] = "Batu", "Blank", jumlah
-                    # arrBahan[2][0], arrBahan[2][1],arrBahan[2][2] = "Air", "Blank", jumlah
-# Contoh [["Pasir","Blank", 40], ["Batu","Blank","30"],["Air","Blank","50"]]
 
 
 
