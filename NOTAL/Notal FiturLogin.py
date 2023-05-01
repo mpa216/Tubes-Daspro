b2f9874bfcdc4bcd2204ae3835dc03de42a19a6e
William <115348219+Azekhiel@users.noreply.github.com>
2023-05-02 03:16:11 +0700

    Update Notal FiturLogin.py

--- NOTAL/Notal FiturLogin.py
+++ NOTAL/Notal FiturLogin.py
@@ -2,17 +2,21 @@ procedure Login()
   global arrUser
   global useraktif
   global roleaktif
-  input(username_login, password_login)
-  if useraktif!="" then
-    output("Anda telah login dengan username tersebut, silahkan lakukan “logout” sebelum melakukan login kembali.")
-  while useraktif=="" and roleaktif=="" do
-    i <- 1
+  global cobalogin
+  udhlogin <- False
+  while not udhlogin do
+    i <- 1 {indeks pertama array =1}
+    input(username_login,password_login)
     iterate
-     if arrUser[i][0]=username_login then
-        if arrUser[i][1]=password_login then
-          kondisi <- ("Selamat datang, Masukkan command “help” untuk daftar command yang dapat kamu panggil.")
-          useraktif <- arrUser [i][1]
-          roleaktif <- arrUser [i][3]
+     if arrUser[i][1]=username_login then
+        if arrUser[i][2]=password_login then
+          cobalogin <- cobalogin + 1
+          if cobalogin>1 then
+            kondisi <- ("Logout terlebih dahulu")
+          else
+            kondisi <- ("Selamat datang, Masukkan command “help” untuk daftar command yang dapat kamu panggil.")
+            useraktif <- arrUser [i][1]
+            roleaktif <- arrUser [i][3]
           stop (arrUser[i][1]=password_login)
         else
           kondisi <- ("Password salah!")
@@ -22,5 +26,3 @@ procedure Login()
       else
         kondisi <- ("Username tidak terdaftar!")
     output(kondisi)
-    if kondisi = "Password salah!" or kondisi = "Username tidak terdaftar!" then
-      input(username_login, password_login)
