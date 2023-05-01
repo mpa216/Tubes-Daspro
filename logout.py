b17902f86bf269d14e7c22037d9bc5b0921ddfaf
dinaasvtr <130291436+dinaasvtr@users.noreply.github.com>
2023-05-02 00:19:27 +0700

    Update logout.py

--- logout.py
+++ logout.py
@@ -1,8 +1,10 @@
-userAktif = ""
+userAktif = "" #menentukan role user saat ini
 
-def logout():
+def logout(): #mendefinisikan fungsi logout
     global userAktif
+    #jika user belum memiliki role, berarti belum login sehingga belum bisa logout
     if userAktif == "" :
         print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
+    #jika user sudah memiliki role, maka role tersebut akan dikosongkan
     else :
-        userAktif = ""
+        userAktif = "" #user berisi string kosong yang berarti sudah berhasil logout
