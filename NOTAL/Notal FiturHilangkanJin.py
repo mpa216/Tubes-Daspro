bd8b1d07ebde48c540103c13540218bd4db876da
William <115348219+Azekhiel@users.noreply.github.com>
2023-05-02 04:33:54 +0700

    Update Notal FiturHilangkanJin.py

--- NOTAL/Notal FiturHilangkanJin.py
+++ NOTAL/Notal FiturHilangkanJin.py
@@ -3,11 +3,11 @@ procedure HilangkanJin():
   global arrCandi
   input(jin_hilang)
   kondisi <- False
-  i<-1
-  iterate (not kondisi) or x<103 do
-    if arrUser[i][1] = jin_hilang then
+  i<-0 {indeks pertama = 0}
+  iterate
+    if arrUser[i][0] = jin_hilang then
       kondisi <- True
-    stop (kondisi = True)
+    stop (kondisi = True or i=102)
     i <- i+1
   if kondisi then
     input(hilang)
@@ -15,14 +15,16 @@ procedure HilangkanJin():
       output("Input salah.")
       input(hilang)
     if  hilang='Y' or hilang='y'then
-      j <- 3
+      j <- 2
       repeat 99 times
         arrUser[j] <- arrUser[j+1]
-      arrUser[102] <- ['inf','inf','inf']
+        j <- j+1
+      arrUser[101] <- ['inf','inf','inf']
 
-      j <- 1
+      j <- 0
       repeat 100 times
-        if arrCandi[j][2]=jin_hilang then
+        if arrCandi[j][1]=jin_hilang then
           arrCandi[j] <- ['inf','inf','inf','inf','inf']
+          j <- j+1
     else
       output("Jin tidak jadi dihilangkan.")
