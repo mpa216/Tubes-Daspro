4db323a7d32d2600fdd42e044ce44b2461be84fc
William <115348219+Azekhiel@users.noreply.github.com>
2023-05-02 04:30:35 +0700

    Update Notal FIturUbahTipe.py

--- NOTAL/Notal FIturUbahTipe.py
+++ NOTAL/Notal FIturUbahTipe.py
@@ -2,19 +2,19 @@ procedure UbahTipe()
   global arrUser
   ada <- False
   input(jinygdiubah)
-  i <- 1
+  i <- 0 {indeks pertama=0}
   while (not ada) or i<103 do
-    if arrUser[i][1] = jinygdiubah then
+    if arrUser[i][0] = jinygdiubah then
       ada <- True
     i <- i+1
   if ada then
-    if arrUser[i-1][3] = "jin_pembangun" then
+    if arrUser[i-1][2] = "jin_pembangun" then
       tipe = "jin_pengumpul"
     else
       tipe = "jin_pembangun"
     input(masukan)
     if masukan='Y' or masukan='y'then
-      arrUser[i-1][3]<-tipe
+      arrUser[i-1][2]<-tipe
       output("Jin telah berhasil diubah.")
     else if masukan=="N" or masukan=="n" then
       output("Jin tidak jadi diubah.")
